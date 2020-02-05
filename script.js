6e3093c0c5c8d19cdb4b6b15ffae877c03ca518b
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-05 15:55:43 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let favMovie = (movie = "Pan's Labyrinth", name = "Rachel") => console.log(`my name is ${name} and my favorite movie is ${movie}`);
+
+favMovie();
+
+let getFirstName = (pizza) => console.log (pizza.split(' ',[1]))
+let getFirstNameConcise = (pizza) => console.log (pizza.split(' ',[1]))
+getFirstNameConcise("Rachel Ruehmer");
+getFirstName("Rachel Ruehmer");
+
